68f1fee4eaa3b1a4997db9eed5c470a91d7136b8
tnmy <tnmykhot@gmail.com>
2021-12-05 20:32:55 -0500

    Added data and trainng results

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-# Run the program: python main.py "Location of training folder" "Mode"
+# Run the program: python main.py data/train train
 
 import cv2
 import os
-from my_utils import *
+from utils import *
 import sys
 
 path = sys.argv[1]
 mode = sys.argv[2]
 
 if mode == "train":
-    os.mkdir('train_features')
+    os.mkdir('results')
+    os.mkdir('results/train_features')
     images,filenames = load_images_from_folder(path)
 
     for i in range(len(images)):
         img = images[i]
-        filename = './train_features/' + filenames[i][0] + '_features.txt'
+        filename = './results/train_features/' + filenames[i][0] + '_features.txt'
         gradient_magnitude, horizontal_gradient, vertical_gradient = compute_gradients(img)
         gradient_angle = compute_gradient_angles(vertical_gradient, horizontal_gradient)
         hog_vector = compute_hog_feature(gradient_magnitude, gradient_angle, cell_size=8, step_size=8, block_size=16, bins=9)
