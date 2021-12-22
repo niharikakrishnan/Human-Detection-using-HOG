ee03312c2b7e0a0b65a5cb0bafe48f1a7debce4c
Niharika Krishnan <niharikakrishnan@gmail.com>
2021-12-22 03:13:27 -0500

    Modified Update Bin Logic and Code Refactoring

---
+++ main_final.py
@@ -0,0 +1,40 @@
+# Project: Human Detection using HOG
+# Run the program: python main.py "Filepath/Human-Detection-using-HOG" "train"
+# Run the program: python main.py "Filepath/Human-Detection-using-HOG" "test"
+
+from utils_done import *
+import sys
+
+# Takes in input as argument from the user
+path = sys.argv[1]
+mode = sys.argv[2]
+
+# If mode is train, positive and negative training images are read and HOG feature vector is calculated
+if mode.lower() == "train":
+    if not os.path.isdir('results'):
+        os.mkdir('results')
+        os.mkdir('results/train_features')
+        os.mkdir('results/test_features')
+    train_images, filenames = load_images_from_folder(path + '/data/train/')
+
+    for i in range(len(train_images)):
+        img = train_images[i]
+        filename = path + '/results/train_features/' + list(filenames.keys())[i] + '_features.txt'
+        get_hog_feature(img, filename)
+
+# If mode is train, test images are loaded, HOG Feature Vector is calculated and the test image is classified as Human or No-Human
+if mode.lower() == "test":
+    train_images, train_files = load_images_from_folder(path + '/data/train/')
+    test_images, test_files = load_images_from_folder(path + '/data/test/')
+
+    train_feature_path = 'results/train_features/'
+    test_feature_path = 'results/test_features/'
+
+    for i in range(len(test_images)):
+        img = test_images[i]
+        filename = path + '/results/test_features/' + list(test_files.keys())[i] + '_features.txt'
+        get_hog_feature(img, filename)
+
+    classification = get_nearest_neighbour(train_files, test_files, train_feature_path, test_feature_path)
+    for key, values in classification.items():
+        print(key, ", Prediction: " + values['Prediction'], ", Actual: " + values['Actual'])
